1b13e93f41a6fa5aaebbf0192315fff72c9abf3d
sman43 <kennethannan9951@gmail.com>
2025-02-08 11:35:00 -0500

    First Submission

---
+++ P1LAB1_AnnanKenneth.py
@@ -0,0 +1,25 @@
+#Kenneth Annan
+#2/4/25
+#P1LAB1
+#This program is a demo for Python
+
+
+#I wasn't here on the 6th, so I had to look up how to tie an input to a variable, and how to print a variable.
+
+first=input("Enter your first name:")
+last=input("Enter your last name:")
+#print("Hello, "+ first + last 'Welcome to CTI110!')
+name = "Alice"
+print("Hello, ", first, last,",Welcome to CTI110!") 
+
+
+
+
+
+
+
+
+
+
+# print("Kenneth")
+# print("Annan")
